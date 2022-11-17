c14b19eb5558e5616b10606d490a5bf88418dd8a
ShivaGanapathy <47366124+ShivaGanapathy@users.noreply.github.com>
2022-11-17 11:46:35 -0500

    Update two_sum.cpp

--- two_sum.cpp
+++ two_sum.cpp
@@ -15,4 +15,32 @@ public:
         return res;
         
     }
+    
+    
+   // Solution 2: Hashmap
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        // We want to make a vector to store the result of the function
+        vector<int> res;
+        
+        // We want to create a hashmap to store complements
+        unordered_map<int, int> hashmap;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            
+            int complement = target - nums[i];
+            
+            // If we haven't seen the complement yet, add the current number
+            if (hashmap.find(complement) == hashmap.end()) {
+                hashmap[nums[i]] = i;
+            }
+            // If we have seen the complement before, we are ready to return
+            else {
+                res.push_back(hashmap[complement]);
+                res.push_back(i);
+                return res;
+            }
+        }
+        
+        return res;
 };
